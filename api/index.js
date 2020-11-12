766e8e5c7ad9d8e9ee49b23ec36621acc893812a
Anton Ivlev <anton.ivlev97@gmail.com>
2020-11-12 16:33:02 +0000

    Add peerserver

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
+import { PeerServer } from "peer";
+
 const endpoint = (req, res) => {
   res.json({
     someKey: "this here is a value from server",
   });
 };
 
-export default endpoint;
+const peerServer = PeerServer({ port: 9000, path: "/myapp" });
+
+export default peerServer;
